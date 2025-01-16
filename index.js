f0aaec74f94fb96d9ee6d71ed512236a116889f2
Shubhamtande <shubhamtande2002@gmail.com>
2025-01-16 16:51:23 +0000

    mycommit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+
+
+var app = express();//Respond with "hello world" for requests that hit our >
+
+app.get('/', function (req, res) {
+
+ res.send('this is for shubham ');
+
+});//listen to port 3000 by default
+
+app.listen(process.env.PORT || 3000);
+
+
+
+module.exports = app;
+
